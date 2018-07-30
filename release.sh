546a924dac1ea4461f175f935d6b68ea2ad4888b
deimo <jadger@163.com>
2018-07-30 16:51:42 +0800

    update - 2018-07-30 16:51:42

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [$1]; then
+    git add .
+    git commit -m $1
+else
+    git add .
+    git commit -m "update - `date '+%Y-%m-%d %T'`"
+fi
+git push origin master
+git push gnu master
+
